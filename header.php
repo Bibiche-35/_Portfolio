464d467f46cd0a20b6795ad7204d7843477f5104
Bibiche-35 <fabrice.hure.35@gmail.com>
2021-04-16 13:52:59 +0200

    Mise en forme avec template et content

--- header.php
+++ header.php
@@ -3,9 +3,9 @@
     <div id="navigation" class="navigation">
         <ul>
             <li><a href="index.php">Home</a></li>
-            <li><a href="portfolio.html">Portfolio</a></li>
-            <li><a href="curriculum.html">Curriculum vitae</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="portfolio.php">Portfolio</a></li>
+            <li><a href="curriculum.php">Curriculum vitae</a></li>
+            <li><a href="contact.php">Contact</a></li>
             <li id="threeline-icon" class="threeline-icon" onclick="openNav()">&Congruent;</li>
         </ul>
     </div>
