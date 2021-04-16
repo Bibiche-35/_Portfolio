75266c11e879d392a49cf8fd8ca1c64d5df94375
Bibiche-35 <fabrice.hure.35@gmail.com>
2021-04-16 13:18:40 +0200

    Première version et comilation des fichiers

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!-- On retrouve ici le template du footer -->
+<footer>
+    <div id="navigation" class="navigation">
+        <ul>
+            <li><a href="mailto:fabrice.hure.35@gmail.com" title="Envoyez-moi un e-mail !">Me contacter</a></li>
+            <li><a class="backlink" href="index.php">Retour à la page d'accueil<a></li>
+            <li>Copyright Fabrice HURÉ - Tous droits réservés</li>
+        </ul>
+    </div>
+</footer>
---
+++ header.php
@@ -0,0 +1,12 @@
+<!-- On retrouve ici le template du header -->
+<header>
+    <div id="navigation" class="navigation">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="portfolio.html">Portfolio</a></li>
+            <li><a href="curriculum.html">Curriculum vitae</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li id="threeline-icon" class="threeline-icon" onclick="openNav()">&Congruent;</li>
+        </ul>
+    </div>
+</header>
