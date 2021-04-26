5e536f946f6bcb71db090349bb289b17214a9df3
Bibiche-35 <fabrice.hure.35@gmail.com>
2021-04-26 11:21:35 +0200

    Renseignement de l'auto formation

---
+++ data/auto-formation.php
@@ -0,0 +1,90 @@
+<?php
+
+$dataArticleList = [
+
+    1 => [
+    "title" => "Application BABBEL",
+    "category" => "Anglais",
+    "categoryColor" => "Anglais" ,
+    "content" =>  "Abonnement depuis le 2 Juin 2020 pour réaliser une remise à niveau avec perfectionnement. Des millions de personnes utilisent Babbel : l’application des experts linguistiques ! Toutes les leçons sont basées sur des conversations réelles, vous aidant à apprendre une langue et à la parler dès le début. 
+    Pourquoi apprendre une langue avec les cours Babbel ?
+    ÉCOUTEZ, PARLEZ, ÉCRIVEZ : suivez des cours préparés sur mesure pour les francophones.
+    UNE SITUATION, UN COURS : apprenez à parler anglais sur des sujets variés tels que les voyages et le travail.
+    MISEZ SUR L’EXPRESSION ORALE : pratiquez et améliorez votre prononciation avec la reconnaissance vocale.",
+    "img" => "/image/babbel.jpg",
+    "link" => "https://my.babbel.com/dashboard",
+    ],
+
+    2 => [
+    "title" => "Application BBC Learning",
+    "category" => "Anglais",
+    "categoryColor" => "Anglais" ,
+    "content" =>  "Sans abonnement, Application gratuite. Apprendre et approfondir les connaissances de la langue anglaise depuis votre smartphone grâce à BBC Learning English pour Android. Scindée en trois grandes sections, cette application dispose pas moins de 400 leçons rangées par années de 2008 à aujourd’hui. 
+    BBC Learning English est principalement centré sur l’écoute audio de conversations, d’interviews et de textes lus avec pour chaque leçon une retranscription complète du texte pour une meilleure compréhension. 
+    Un système de playlists est également disponible avec la possibilité de pouvoir télécharger des leçons sur votre mobile afin de pouvoir les écouter en mode hors ligne. Simple d’utilisation et riche en contenus BBC Learning English est une façon originale d’apprendre la langue de Shakespeare",
+    "img" => "/image/BBCLearning.jpg",
+    "link" => "https://www.bbc.co.uk/search?q=English+at+work",
+    ],
+
+    3 => [
+    "title" => "OPEN CLASS ROOM",
+    "category" => "Informatique",
+    "categoryColor" => "Informatique" ,
+    "content" =>  "Sans abonnement, Application gratuite. OpenClassrooms est un site web de formation en ligne qui propose à ses membres des cours certifiants et des parcours débouchant sur des métiers en croissance. 
+    C’est une plateforme de formation qui propose des cours en ligne dans différents domaines liés à l’informatique (design, conception, projet, entreprenariat, …).
+    Ses contenus sont réalisés en interne, par des écoles, des universités, des entreprises partenaires comme Microsoft ou IBM, ou historiquement par des bénévoles.",
+    "img" => "/image/OpenClassRoom.jpg",
+    "link" => "https://openclassrooms.com/fr/dashboard/courses#courses-taken",
+    ],
+
+    4 => [
+    "title" => "Formation E-Santé",
+    "category" => "E-santé",        
+    "categoryColor" => "Santé" ,
+    "content" =>  "Sans abonnement, Application gratuite. Apprendre et approfondir les connaissances de la langue anglaise depuis votre smartphone grâce à BBC Learning English pour Android. Scindée en trois grandes sections, cette application dispose pas moins de 400 leçons rangées par années de 2008 à aujourd’hui. 
+    BBC Learning English est principalement centré sur l’écoute audio de conversations, d’interviews et de textes lus avec pour chaque leçon une retranscription complète du texte pour une meilleure compréhension. 
+    Un système de playlists est également disponible avec la possibilité de pouvoir télécharger des leçons sur votre mobile afin de pouvoir les écouter en mode hors ligne. Simple d’utilisation et riche en contenus BBC Learning English est une façon originale d’apprendre la langue de Shakespeare",
+    "img" => "/image/BBCLearning.jpg",
+    "link" => "https://www.bbc.co.uk/learningenglish/",
+    ],
+
+
+    5 => [
+    "title" => "MIMO",
+    "category" => "Informatique",
+    "categoryColor" => "Informatique" ,
+    "content" =>  "Sans abonnement, Application gratuite. Apprendre et approfondir les connaissances de la langue anglaise depuis votre smartphone grâce à BBC Learning English pour Android. Scindée en trois grandes sections, cette application dispose pas moins de 400 leçons rangées par années de 2008 à aujourd’hui. 
+    BBC Learning English est principalement centré sur l’écoute audio de conversations, d’interviews et de textes lus avec pour chaque leçon une retranscription complète du texte pour une meilleure compréhension. 
+    Un système de playlists est également disponible avec la possibilité de pouvoir télécharger des leçons sur votre mobile afin de pouvoir les écouter en mode hors ligne. Simple d’utilisation et riche en contenus BBC Learning English est une façon originale d’apprendre la langue de Shakespeare",
+    "img" => "/image/BBCLearning.jpg",
+    "link" => "https://www.bbc.co.uk/learningenglish/",
+    ],
+
+            
+    6 => [
+    "title" => "SOLOLEARN",
+    "category" => "Informatique",
+    "categoryColor" => "Informatique" ,
+    "content" =>  "Sans abonnement, Application gratuite. Apprendre et approfondir les connaissances de la langue anglaise depuis votre smartphone grâce à BBC Learning English pour Android. Scindée en trois grandes sections, cette application dispose pas moins de 400 leçons rangées par années de 2008 à aujourd’hui. 
+    BBC Learning English est principalement centré sur l’écoute audio de conversations, d’interviews et de textes lus avec pour chaque leçon une retranscription complète du texte pour une meilleure compréhension. 
+    Un système de playlists est également disponible avec la possibilité de pouvoir télécharger des leçons sur votre mobile afin de pouvoir les écouter en mode hors ligne. Simple d’utilisation et riche en contenus BBC Learning English est une façon originale d’apprendre la langue de Shakespeare",
+    "img" => "/image/BBCLearning.jpg",
+    "link" => "https://www.bbc.co.uk/learningenglish/",
+    ],
+
+    7 => [
+    "title" => "FUN MOOC",
+    "category" => "Projets",
+    "categoryColor" => "Projets" ,
+    "content" =>  "Dans ce MOOC « Du manager au leader, devenir agile et collaboratif », qui s'adresse à tous, quel que soit votre métier, quel que soit votre secteur d'activité, on vous donnera les principes de base pour savoir manager à l'ère du numérique. L'idée, c'est de vous donner les compétences de base, les compétences numériques pour comprendre cette civilisation. Ensuite, les compétences d'agilité pour travailler différemment. Les compétences de design thinking pour innover au quotidien et, bien sûr, cette année en exclusivité, les compétences de collaboration pour créer de l'intelligence collective et être encore plus performant demain, quand vous allez devoir transformer votre métier avec l'intelligence artificielle. 
+    Vous l'avez bien compris, ce MOOC est une brique essentielle pour savoir manager.",
+    "img" => "/image/managerauleader.jpg",
+    "link" => "https://lms.fun-mooc.fr/dashboard",
+    ],
+
+];
+
+
+
+
+?>
---
+++ data/veille.php
@@ -0,0 +1,98 @@
+<?php
+
+$dataArticleList = [
+
+    1 => [
+    "title" => "Lorem ipsum dolor article 1",
+    "author" => "Jean Bulldog",
+    "content" =>  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi accusantium iure odio fugiat?
+    Inventore ab alias et! Ex, nesciunt suscipit corporis tempore molestias odio assumenda
+    temporibus quo cumque quibusdam inventore voluptatem debitis quia facilis! Culpa nisi temporibus
+    quae repudiandae harum labore vero dolorum inventore aspernatur, ad rerum, natus delectus vel!
+    Aspernatur mollitia, molestiae ipsum, accusamus eum architecto a aliquam quisquam ipsa dicta
+    optio! Harum, adipisci fugiat nulla provident culpa eligendi? Ducimus nostrum quis beatae amet
+    perspiciatis, impedit, rem ipsam non aut hic quam dolorum excepturi ea, architecto ad fuga.
+    Beatae possimus non accusamus. Illum, inventore. Maxime qui omnis dolore. Enim placeat suscipit
+    nobis excepturi iure, veniam rem neque voluptas quam cumque officiis iste est dolores officia,
+    optio aspernatur fugiat ex ab dolore vitae ipsum ipsam! Vel numquam aliquid explicabo ipsa
+    repellendus odit adipisci iure! Optio odio nesciunt ea temporibus reiciendis alias
+    exercitationem dicta, quis velit beatae enim tenetur doloremque. Ut corrupti, quibusdam
+    perferendis nisi magni autem repellat eum sint. Reprehenderit dicta veritatis earum! Animi
+    consequuntur explicabo labore cupiditate eum quidem quo, voluptatem culpa autem quod corrupti
+    dignissimos similique molestiae voluptatum dolores, eveniet aliquam rem possimus aut ipsum,
+    maiores nostrum mollitia? Quisquam, dolor eum? Aliquam sapiente omnis, distinctio sunt cumque
+    quas culpa quos similique reiciendis ipsam id velit soluta quod minima delectus quasi ipsa
+    debitis animi? Nobis aliquid repellendus dignissimos in fugiat laboriosam facilis officiis
+    veniam molestias. Cupiditate non maiores eum adipisci praesentium officiis suscipit? Quisquam
+    dignissimos facere quam aliquid corrupti eligendi aliquam dolorem mollitia, maxime recusandae
+    illo obcaecati nisi blanditiis numquam nesciunt consequatur quae nihil accusantium. Ad eligendi
+    quibusdam velit, eos at aliquid a. Nostrum velit tempora accusantium beatae quod fugit maiores
+    et reprehenderit porro veritatis, qui sint mollitia, possimus iusto necessitatibus repudiandae
+    reiciendis deleniti voluptatibus! Ratione iusto explicabo sit consequatur consectetur ducimus
+    cupiditate praesentium, voluptate magni animi reiciendis, voluptates libero reprehenderit saepe
+    eius quos nam illo ab officia doloremque corporis excepturi aliquam! Aperiam natus distinctio
+    totam dolorum, facilis esse accusamus amet repudiandae quis. Quia perspiciatis, sint dolores
+    reiciendis, quibusdam natus a nemo beatae, quam temporibus laudantium dolorum iste! Culpa quas
+    nobis esse vitae veniam sint ex pariatur neque placeat laborum, perspiciatis nihil tempora amet
+    architecto recusandae sit impedit magni iste quod eius autem rem consequuntur vel eos.
+    Aspernatur fugit voluptate voluptas aut quisquam exercitationem sapiente possimus aliquam
+    blanditiis sequi? Accusamus, dignissimos expedita tempore sed, aliquam iure tempora nobis quae
+    perferendis, suscipit delectus saepe. Praesentium rem ipsa iure vitae molestias.",
+    "category" => "Education",
+    "categoryColor" => "education" ,
+    "date" => "le 22 mars 2020",
+    "imgauthor" => "../images/profil-avatar-homme-icone-ronde.jpg"
+    ],
+
+    2 => [
+    "title" => "Lorem ipsum dolor article 2",
+    "author" => "Claude Teckel",
+    "content" =>  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos id impedit at quo, expedita iure, molestias error ullam distinctio vel.",
+    "category" => "Bien-être",
+    "categoryColor" => "care" ,
+    "date" => "le 14 février 2020",
+    "imgauthor" => "../images/profil-avatar-beagles.png" 
+    ],
+    3 => [
+    "title" => "Lorem ipsum dolor article 3",
+    "author" => "Emma Rottweiler",
+    "content" =>  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Location magnam, distinctio! Vero blanditiis nisi veritatis id perferendis, quod quaerat necessitatibus ullam nobis quidem ipsam optio sit quibusdam ad quae voluptatibus doloremque!",
+    "category" => "Bien-être",
+    "categoryColor" => "care" ,
+    "date" => "le 30 janvier 2020",
+    "imgauthor" => "../images/profil-avatar-femme-icone-ronde-rose.jpg"
+    ],
+    4 => [
+    "title" => "Lorem ipsum dolor article 4",
+    "author" => "Mathilde Beagles",
+    "content" =>  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pirates molestias molestiae, obcaecati et. Veritatis, laudantium!",
+    "category" => "Loisirs",
+    "categoryColor" => "fun" ,
+    "date" => "le 15 janvier 2020",
+    "imgauthor" => "../images/profil-avatar-femme-icone-ronde.jpg"
+    ],
+    5 => [
+    "title" => "Lorem ipsum dolor article 5",
+    "author" => "Charles Bouvier",
+    "content" =>  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Celtics nostrum obcaecati sit odit eveniet, unde hic animi molestiae, odio a quia aut perspiciatis dicta.",
+    "category" => "Education",
+    "categoryColor" => "education" ,
+    "date" => "le 20 décembre 2019",
+    "imgauthor" => "../images/profil-avatar-homme-icone-ronde-orange.jpg"
+    ],
+    6 => [
+    "title" => "Lorem ipsum dolor article 6",
+    "author" => "Claude Teckel",
+    "content" =>  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nascar vitae assumenda reiciendis doloremque, quisquam.",
+    "category" => "Loisirs",
+    "categoryColor" => "fun" ,
+    "date" => "le 25 octobre 2019",
+    "imgauthor" => "../images/profil-avatar-beagles.png"
+    ],
+
+    ];
+
+
+
+
+?>
